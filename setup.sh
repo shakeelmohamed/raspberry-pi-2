43994551afbaa22a8adae8fa3abdb64f7526a699
Shakeel Mohamed <shakeel_mohamed@live.com>
2015-10-02 01:24:39 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ git clone git@github.com:shakeelmohamed/dotfiles.git
 ## NVM, abstracts the mess of compiling Node.js from source
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
 nvm install 0.10
-node use 0.10
+nvm use 0.10
 
 # Python
 ## pyenv
