f6c49f9567c96246f09a3d4e38677b8c16ba741e
Shakeel Mohamed <shakeel_mohamed@live.com>
2015-10-02 00:15:02 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,14 +7,17 @@ sudo su
 apt-get install tightvncserver
 cp ./etc/init.d/vncboot /etc/init.d/vncboot
 
+# git
+apt-get install git
+# TODO: pull in dot files, and source the great things in shak.sh
+# git clone git@github.com:shakeelmohamed/dotfiles.git
 
-# Node.js setup
-
+# Node.js
 ## NVM, abstracts the mess of compiling Node.js from source
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
 nvm install 0.10
 
-# git
-apt-get install git
-
-# TODO: pull in dot files, and source the great things in shak.sh
+# Python
+## pyenv
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+printf 'export PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ~/.bashrc
