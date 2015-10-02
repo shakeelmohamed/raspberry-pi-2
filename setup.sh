6546398b768b9aaccc892d9c0099cd1ecca22e9a
Shakeel Mohamed <shakeel_mohamed@live.com>
2015-10-01 23:55:53 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ sudo su
 # VNC Server setup, connect via <ip-address>:1
 apt-get install tightvncserver
 cp ./etc/init.d/vncboot /etc/init.d/vncboot
+
+# Node.js setup
+
+## NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
