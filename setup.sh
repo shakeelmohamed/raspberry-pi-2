1cf53ed9570d835862bbcebe5e5c133da0555a8a
Shakeel Mohamed <shakeel_mohamed@live.com>
2015-10-02 00:03:39 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# TODO: make running this script setup a full environment
+# TODO: make running this script setup a full environment from scratch
 
 # Just ask for sudo once
 sudo su
@@ -7,8 +7,14 @@ sudo su
 apt-get install tightvncserver
 cp ./etc/init.d/vncboot /etc/init.d/vncboot
 
+
 # Node.js setup
 
-## NVM
+## NVM, abstracts the mess of compiling Node.js from source
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
 nvm install 0.10
+
+# git
+apt-get install git
+
+# TODO: pull in dot files, and source the great things in shak.sh
